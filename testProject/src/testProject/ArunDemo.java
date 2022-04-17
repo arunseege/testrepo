23be76491405d86b20b46669661ba82c5aabe1fb
arunseege <arunsphassan@gmail.com>
2022-04-17 16:20:56 +0530

    first feature arundemo

---
+++ testProject/src/testProject/ArunDemo.java
@@ -0,0 +1,10 @@
+package testProject;
+
+public class ArunDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("first feature added now");
+	}
+
+}
